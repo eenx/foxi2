0840dcd1fccfa681753a9eb87aa100af9f6dfb30
eenx <eenx@outlook.com>
2025-02-17 21:21:33 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app/",
+  site: "https://eenx.github.io/",
+  base: 'foxi2',
   integrations: [
     tailwind(),
     icon(),
